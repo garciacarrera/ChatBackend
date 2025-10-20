762fd92d10741fb8456230d62ad2376d55de5a98
Lucas.g.c <fxtr088@gmail.com>
2025-10-20 18:07:07 -0300

    creo variables de entorno

---
+++ src/configuration/envs.js
@@ -0,0 +1,31 @@
+import joi from 'joi';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    DATABASE: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USER: joi.string().required(),
+    DB_PASS: joi.string().allow('').optional(),
+    DB_HOST: joi.string().required(),
+    
+  })
+  .unknown(true);
+
+const { value, error } = envsSchema.validate(process.env);
+
+if (error) throw new Error(error.message);
+
+export const envs = {
+  PORT: value.PORT,
+  DATABASE: value.DATABASE,
+  DB_PORT: value.DB_PORT,
+  DB_USER: value.DB_USER,
+  DB_PASS: value.DB_PASS,
+  DB_HOST: value.DB_HOST,
+ 
+};
