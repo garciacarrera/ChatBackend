c94530d57a515c219c6ba84223776b67f3c2916b
Lucas.g.c <fxtr088@gmail.com>
2025-10-21 21:36:41 -0300

    creacion de entidad participa

---
+++ src/module/entity/Participa_entity.js
@@ -0,0 +1,31 @@
+const { EntitySchema } = require("typeorm");
+
+module.exports = new EntitySchema({
+  name: "Participa",
+  tableName: "participa",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true
+    }
+  },
+  relations: {
+    user: {
+      type: "many-to-one",
+      target: "User",
+      joinColumn: { name: "userId" },
+      nullable: false,
+      onDelete: "CASCADE",
+      inverseSide: "participaciones"
+    },
+    chat: {
+      type: "many-to-one",
+      target: "Chat",
+      joinColumn: { name: "chatId" },
+      nullable: false,
+      onDelete: "CASCADE",
+      inverseSide: "participaciones"
+    }
+  }
+});
