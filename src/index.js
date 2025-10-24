350ba11ddd9f83f13f0d6e6277279b24ed00caa9
Golver2909 <matiasfuentes2909@gmail.com>
2025-10-24 20:24:34 -0300

    Emits añadidos

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import app from "./app.js";
+import { envs } from "./configuration/envs.js";
 
-const app = require("./app.js")
-const envs = require("./configuration/envs.js")
-
-
+//Creamos una clase http que requiere 'http'
 const http = require("http")
 
+//Creamos un servidor usando el archivo "app.js"
 const server = http.createServer((app))
 
+//Llamamos a la clase 'Server' de socket.io para instanciar el servidor
 const { Server } = require("socket.io")
-
 const io = new Server(server)
 
 const main = () =>{
-    const port = app.get('port')
 
+    //Evento de conexion(ejecutado al iniciar la aplicacion)
     io.on("connection",(socket) =>{
-        console.log("Succesful connection")
+        console.log("Conexion exitosa")
+
+        console.log(socket)
     })
 
-    server.listen(app.get("port"))
-    console.log(`server running on port ${port}`)
+    server.listen(app.get('port'))
+    console.log(`server running on port ${envs.PORT}`)
 }
 
 
--- src/websocket/handler.socket.js
+++ src/websocket/handler.socket.js
@@ -1,10 +1,15 @@
 const socketHandler = (socket) =>{
+    //Evento "Message" con la capacidad de brindar un mensaje(muestra los datos del mensaje)
     socket.on("message",(data) =>{
-        console.log("Mensaje 1")
+        console.log("Mensaje:",data)
+
+        socket.emit("message","Mensaje Procesado")
     })
     
     socket.on("disconnect",()=>{
-        console.log("disconnected")
+        console.log("No hay un usuario conectado")
+
+        socket.emit("disconnect","Desconexion")
     })
 }
 
