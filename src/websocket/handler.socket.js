4101b397be8afc390bbc72313650076c79934f1a
Golver2909 <matiasfuentes2909@gmail.com>
2025-10-27 12:35:48 -0300

    Evento de logueo añadido correctamente al socketHandler

--- src/websocket/handler.socket.js
+++ src/websocket/handler.socket.js
@@ -6,10 +6,18 @@ const socketHandler = (socket) =>{
         socket.emit("message","Mensaje Procesado")
     })
     
+    //Evento para la desconexion de usuarios, o en caso de falta de conexion
     socket.on("disconnect",()=>{
         console.log("No hay un usuario conectado")
 
         socket.emit("disconnect","Desconexion")
+    })
+
+    //Evento de logueo de usuarios
+    socket.on("login", ()=>{
+        console.log("Usuario conectado correctamente")
+
+        socket.emit("login","Conexion de usuario realizada correctamente")
     })
 }
 
